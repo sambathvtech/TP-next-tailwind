5d262a2f4fb9752b6bfd0a7d503563f54fd7de59
Try Sambath <97923193+sambathvtech@users.noreply.github.com>
2022-07-06 20:11:15 +0700

    update

--- src/components/Layout/container.js
+++ src/components/Layout/container.js
@@ -1,12 +1,21 @@
 import React from 'react';
 
-const ContainerDefualt = (children) => {
-  return <div className='p-5 pt-7 space-y-4 xl:w-4/5 2xl:w-2/5 mx-auto mb-8'>{children}</div>;
+const ContainerDefualt = ({
+  children,
+  defualtStyles = 'space-y-4 xl:w-4/5 2xl:w-2/5 mx-auto',
+  className = '',
+}) => {
+  return <div className={`${defualtStyles} ${className}`}>{children}</div>;
 };
 
-export default function Container({ type, children }) {
+export default function Container({ type, children, defualtStyles, className }) {
+  const props = {
+    defualtStyles,
+    children,
+    className,
+  };
   switch (type) {
     default:
-      return ContainerDefualt(children);
+      return ContainerDefualt(props);
   }
 }
--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { BiUpArrow } from 'react-icons/bi';
 
 export const MainLayout = ({ children }) => {
   // State
