697408b76eab8324990be76cb0a0f5881dbd7a6f
Try Sambath <97923193+sambathvtech@users.noreply.github.com>
2022-07-21 20:49:13 -0700

    Update dev.js

--- src/utilities/dev.js
+++ src/utilities/dev.js
@@ -1,4 +1,4 @@
-export const isDebug = () => process?.env?.NODE_ENV !== 'production';
+export const isDebug = () => process?.env?.NODE_ENV === 'development';
 
 export const getDomain = () =>
   isDebug() ? 'http://localhost:3000' : process?.env?.NEXT_PUBLIC_API_URL;
